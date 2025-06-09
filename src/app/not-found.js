0bc337c5225907576172f071fa52650142de0389
hiro502 <hiro1887v@gmail.com>
2025-06-09 20:28:47 +0200

    corrige page 404

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -2,7 +2,7 @@ export default function NotFound() {
   return (
     <div className="text-center">
       <h2 className="text-4xl font-bold mb-4">404 - Page not Found</h2>
-      <p className="text-2xl">Cette page n'existe pas.</p>
+      <p className="text-2xl">Cette page n&apos;existe pas.</p>
     </div>
   );
 }
